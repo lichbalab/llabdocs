d42c40b8f18957416d67e2e52f4ccc59438c33e6
kiryl <kiryl.bokiy@gmail.com>
2025-01-17 23:00:24 +0200

    OpenAPI specification added into controller for signature validation

--- modules/llabdocs-rest-api/src/main/java/com/lichbalab/docs/api/model/DocumentSignatureValidationResult.java
+++ modules/llabdocs-rest-api/src/main/java/com/lichbalab/docs/api/model/DocumentSignatureValidationResult.java
@@ -1,14 +1,25 @@
 package com.lichbalab.docs.api.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 
+@Schema(description = "Result of document signature validation")
 public class DocumentSignatureValidationResult {
+    @Schema(description = "Name of the validated document")
     private String documentName;
+    
+    @Schema(description = "List of individual signature validation results")
     private List<SignatureValidationResult> signatures;
+    
+    @Schema(description = "Summary of validation results")
     private ValidationSummary summary;
 
+    @Schema(description = "Summary statistics of signature validation")
     public static class ValidationSummary {
+        @Schema(description = "Total number of signatures in the document")
         private int totalSignatures;
+        
+        @Schema(description = "Number of valid signatures")
         private int validSignatures;
 
         public int getTotalSignatures() {
