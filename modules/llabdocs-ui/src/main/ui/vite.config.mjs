e6841e9624515da0efd7ecad797de59f1e089a51
kiryl <kiryl.bokiy@gmail.com>
2025-08-15 19:25:12 +0300

    minor improvements in logging and deployment

--- modules/llabdocs-ui/src/main/ui/vite.config.mjs
+++ modules/llabdocs-ui/src/main/ui/vite.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/docs": {
-        target: "http://localhost:8080",
+        target: "http://localhost:8081",
         changeOrigin: true
       }
     }
